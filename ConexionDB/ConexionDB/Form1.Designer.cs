856008a17ae2c3844ff4edfa6cace211e8699476
dwn84 <dwn84@hotmail.com>
2020-03-27 16:40:08 -0500

    DB

--- ConexionDB/ConexionDB/Form1.Designer.cs
+++ ConexionDB/ConexionDB/Form1.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descripción";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(105, 35);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(100, 20);
+            this.txtDescripcion.TabIndex = 1;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(105, 83);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Precio";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(84, 127);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Guardar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(296, 190);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Agregar nuevo producto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAgregar;
     }
 }
 
--- ConexionDB/ConexionDB/Form1.cs
+++ ConexionDB/ConexionDB/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//agregar la libreria para gestion de base de datos: sql server
+using System.Data.SqlClient;
 
 namespace ConexionDB
 {
@@ -15,6 +17,40 @@ namespace ConexionDB
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection("server=SYN\\SQLEXPRESS;integrated security=true;database=base1");
+            conexion.Open();
+            //MessageBox.Show("Conexion satisfactoria a la base de datos");
+           
+
+            if (txtDescripcion.Text == "")
+            {
+                MessageBox.Show("No se puede omitir la descripción");
+            }
+            else 
+            {
+
+                String sql = "insert into Articulos(Descripcion,Precio)  values('" + txtDescripcion.Text + "', " + txtPrecio.Text + "); ";
+                
+                SqlCommand ejecutarConsulta = new SqlCommand(sql, conexion);
+                ejecutarConsulta.ExecuteNonQuery();
+                MessageBox.Show("Dato agregado correctamente");
+
+                txtDescripcion.Clear();
+                txtPrecio.Clear();
+                txtDescripcion.Focus();
+
+            }
+
+            conexion.Close();
         }
     }
 }
